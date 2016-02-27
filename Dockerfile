64d8854733ac0f20a9ff42f06309c9cfed46a558
Huseyin Bacanak <hbdevacc@gmail.com>
2016-02-28 00:33:45 +0200

    added docker config

---
+++ Dockerfile
@@ -0,0 +1,8 @@
+FROM rave/developer
+
+
+ADD . /development/workspace/docker1
+
+WORKDIR /development/workspace/docker1
+
+
---
+++ src/main/java/demo/docker1/App.java
@@ -0,0 +1,10 @@
+package demo.docker1;
+
+/**
+ * Created by raven on 27.02.2016.
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
